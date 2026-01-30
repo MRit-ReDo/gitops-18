f9b31b168dcddc2546d1977a9f3b19cd955a25b0
root <root@ip-172-31-71-215.ap-south-1.compute.internal>
2026-01-30 12:29:48 +0000

    push to repo

---
+++ main.tf
@@ -0,0 +1,153 @@
+resource "aws_s3_bucket" "website_bucket" {
+
+
+
+  bucket = "shambhavi-static-site-2026"
+
+
+
+}
+
+
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+
+
+
+  bucket = aws_s3_bucket.website_bucket.id
+
+
+
+
+
+
+
+  block_public_acls       = false
+
+
+
+  block_public_policy     = false
+
+
+
+  ignore_public_acls      = false
+
+
+
+  restrict_public_buckets = false
+
+
+
+}
+
+
+
+resource "aws_s3_bucket_policy" "public_policy" {
+
+
+
+  bucket = aws_s3_bucket.website_bucket.id
+
+
+
+
+
+
+
+  policy = jsonencode({
+
+
+
+    Version = "2012-10-17"
+
+
+
+    Statement = [{
+
+
+
+      Sid       = "PublicReadGetObject"
+
+
+
+      Effect    = "Allow"
+
+
+
+      Principal = "*"
+
+
+
+      Action    = "s3:GetObject"
+
+
+
+      Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+
+
+
+    }]
+
+
+
+  })
+
+
+
+}
+
+
+
+resource "aws_s3_bucket_website_configuration" "website" {
+
+
+
+  bucket = aws_s3_bucket.website_bucket.id
+
+
+
+
+
+
+
+  index_document {
+
+
+
+    suffix = "index.html"
+
+
+
+  }
+
+
+
+}
+
+
+
+resource "aws_s3_object" "index" {
+
+
+
+  bucket       = aws_s3_bucket.website_bucket.id
+
+
+
+  key          = "index.html"
+
+
+
+  source       = "index.html"
+
+
+
+  content_type = "text/html"
+
+
+
+}
+
+
+
+
