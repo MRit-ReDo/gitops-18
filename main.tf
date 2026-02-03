098954e37071890ae892e3ea71ad8b5c9cd7ae81
Shambhavi Singh <shambhavi.singh.work@gmail.com>
2026-02-03 15:18:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,153 +1,59 @@
 resource "aws_s3_bucket" "website_bucket" {
-
-
-
   bucket = "shambhavi-gitops-shambhavi-0904"
-
-
-
 }
 
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.website_bucket.id
 
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
-
-
-
   bucket = aws_s3_bucket.website_bucket.id
-
-
-
-
-
-
 
   block_public_acls       = false
-
-
-
   block_public_policy     = false
-
-
-
   ignore_public_acls      = false
-
-
-
   restrict_public_buckets = false
-
-
-
 }
-
-
 
 resource "aws_s3_bucket_policy" "public_policy" {
-
-
-
   bucket = aws_s3_bucket.website_bucket.id
 
-
-
-
-
-
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access,
+    aws_s3_bucket_ownership_controls.ownership
+  ]
 
   policy = jsonencode({
-
-
-
     Version = "2012-10-17"
-
-
-
     Statement = [{
-
-
-
       Sid       = "PublicReadGetObject"
-
-
-
       Effect    = "Allow"
-
-
-
       Principal = "*"
-
-
-
       Action    = "s3:GetObject"
-
-
-
       Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-
-
-
     }]
-
-
-
   })
-
-
-
 }
-
-
 
 resource "aws_s3_bucket_website_configuration" "website" {
-
-
-
   bucket = aws_s3_bucket.website_bucket.id
 
-
-
-
-
-
+  depends_on = [
+    aws_s3_bucket_policy.public_policy
+  ]
 
   index_document {
-
-
-
     suffix = "index.html"
-
-
-
   }
-
-
-
 }
-
-
 
 resource "aws_s3_object" "index" {
-
-
-
   bucket       = aws_s3_bucket.website_bucket.id
-
-
-
   key          = "index.html"
-
-
-
   source       = "index.html"
-
-
-
   content_type = "text/html"
-
-
-
 }
-
-
-
-
